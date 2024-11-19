ba49c21da5ebf7b9c93ff0868921e311dfec769b
Paddy <ohanlon.paddy@gmail.com>
2024-11-19 10:30:54 +0100

    Fix title length in model

--- backend/models/feedback.ts
+++ backend/models/feedback.ts
@@ -7,7 +7,7 @@ const feedbackSchema = new mongoose.Schema(
     name: { type: String, required: true, maxLength: config.get('NAME_MAX_LENGTH') },
     email: { type: String, required: true, maxLength: config.get('EMAIL_MAX_LENGTH') },
     type: { type: String, required: true, enum: Object.values(FeedbackType) },
-    title: { type: String, required: true, maxLength: config.get('MESSAGE_MAX_LENGTH') },
+    title: { type: String, required: true, maxLength: config.get('TITLE_MAX_LENGTH') },
     message: { type: String, required: true, maxLength: config.get('MESSAGE_MAX_LENGTH') }
   },
   { timestamps: true }
