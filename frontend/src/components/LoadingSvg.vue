45414c815eecc00a542d8022f102cc9d558d2abc
Paddy <ohanlon.paddy@gmail.com>
2024-11-19 22:38:27 +0100

    Use custom tailwind colour for loader

--- frontend/src/components/LoadingSvg.vue
+++ frontend/src/components/LoadingSvg.vue
@@ -4,7 +4,7 @@ const { width = '40px' } = defineProps<{ width?: string }>()
 
 <template>
   <svg
-    class="animate-spin"
+    class="animate-spin fill-mint-green"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     :style="`width: ${width}`"
@@ -12,7 +12,6 @@ const { width = '40px' } = defineProps<{ width?: string }>()
   >
     <title id="loading-spinner">Spinning loader icon</title>
     <path
-      fill="#00e29d"
       d="m12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8v-2c-5.421 0-10 4.58-10 10 0 5.421 4.579 10 10 10z"
     ></path>
   </svg>
